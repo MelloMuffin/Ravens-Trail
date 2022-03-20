3087087e42700d66eab4dd543561afbe9332ad6b
mellomuffin <78819366+notDaylight@users.noreply.github.com>
2022-03-20 08:32:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ let coins = 0;
 let friends = 0;
 //Just getting stuff down
 
+textSize(32);
+textAlign(CENTER, CENTER);
+text("Raven's Trail", 200, 50);
+textSize(18);
+text("By MelloMuffin", 200, 90);
+//Title go brr
